c62811d8447beb0e097d5640ae4e102438e0cf13
Haidar Rayan <rayan.haidar@isae.edu.lb>
2022-10-23 23:40:39 +0300

    feat: adding new Transaction

--- src/test/java/com/example/demo/repository/DemoApplicationTests.java
+++ src/test/java/com/example/demo/repository/DemoApplicationTests.java
@@ -3,6 +3,8 @@ package com.example.demo.repository;
 import com.example.demo.model.Client;
 import com.example.demo.model.Compte;
 import static org.assertj.core.api.Assertions.assertThat;
+
+import com.example.demo.model.Transaction;
 import org.junit.Ignore;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +15,8 @@ class DemoApplicationTests {
 
     @Autowired
     ClientDAO clientDao;
+    @Autowired
+    TransactionDAO transactionDAO;
 
     @Test
     @Ignore
@@ -42,6 +46,18 @@ class DemoApplicationTests {
         Client c2 = clientDao.findByEmail("pascal2.fares@lecnam.net");
         assertThat(c2.getFriends().size()).isEqualTo(c1.getFriends().size());
 
+    }
+
+    @Test
+    public void createTransaction(){
+        //Ici j'ai chisit de creer un nouveau client mais aussi je peux faire un findClientByEmail("pascal.fares@lecnam.net")
+        //puis faire la transaction sur ce mail
+        Client c = new Client("Rayan", "Haidar", "rayan.haidar@isae.edu.lb");
+        Compte cpt = new Compte(c, 1000);
+        c.setCompte(cpt);
+        clientDao.save(c);
+        Transaction transaction = new Transaction(1000, c, cpt);
+        transactionDAO.save(transaction);
     }
 
 }
